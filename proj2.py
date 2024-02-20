01edaad32db6451730da4efa2dd892b98e473d44
BirdVulture <zadorozhny80@yandex.ru>
2024-02-20 17:22:50 +0700

    учеба

--- proj2.py
+++ proj2.py
@@ -1,8 +1,9 @@
-# print('Java')
-# print('Ruby')
-# print('Scala')
-print('Python', end='+')  # print('C++')
-# print('GO')
-print('C#', end='=')  # print('C')
-print('awesome')
-# finish
+x = 3 
+y = 4 
+z = x + y 
+z = z + 1 
+x = y 
+y = 5
+x = z + y + 7
+print(x)
+
