fe572e742955c3a0a5b5be55f90f8a87a3bc2546
BirdVulture <zadorozhny80@yandex.ru>
2024-02-25 21:10:22 +0700

    Крестики Нолики

--- Harvard Course.py
+++ Harvard Course.py
@@ -51,12 +51,12 @@ print(z)
 
 '''
 #FUNCTIONS (def) 1:31
-
+'''
 def hello(to):
     print("hello, ", to)  
     
 
-
+hello()
 
 
 def main():
@@ -65,11 +65,103 @@ def main():
     hello(name)
 
 
-def mainTwo()
+def mainTwo():
     x = int(input("What's x? "))
     print("x squared is", square(x))
 
 def square(n): #степнь n ** 2
     return pow(n, 2)
+'''
 
 # Conditionals (условия) 1:50
+'''
+xy = int(input("What's x? "))
+ = int(input("What's y?"))
+
+if x < y:
+    print("x is less than y")
+
+elif x > y:
+    print("x is greater than y")
+
+else:
+    print("x is equal than y")
+
+
+if x < y or x > y:
+    print("x is not equal to y")
+else:
+    print("x is equal than y")
+
+if x != y:
+    print("x is not equal to y")
+else:
+    print("x is equal than y")   
+'''
+    
+'''
+score = int(input("Score: "))
+
+if score >= 90 and score <=100:
+    print("Grade: A")
+elif score >= 80 and score < 90:
+    print("Grade: B")
+elif score >= 70 and score < 80:
+    print("Grade: C")
+elif score >= 60 and score < 70:
+    print("Grade: D")
+else:
+    print("Grade: D")
+
+# This code is better
+if 90 <= score <=100:
+    print("Grade: A")
+elif 80 <= score < 90:
+    print("Grade: B")
+elif 70 <= score < 80:
+    print("Grade: C")
+elif 60 <= score < 70:
+    print("Grade: D")
+else:
+    print("Grade: D")
+
+# This code is more better
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
+else:
+    print("Grade: D")
+'''
+# Parity
+
+x = int(input("What's x? "))
+y = int(input("What's y?"))
+
+if x % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+def main():
+    x = int(input("What's x?"))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
+
+
+# Even or odd (четное/нечетное) 
+def is_even(n):
+    if n % 2 ==0:
+        return True
+    else:
+        return False
+    
+
+
+    
--- Proj.py
+++ Proj.py
@@ -1,27 +1,94 @@
+import tkinter as tk
 
-def turn():
-    x1 = int(input("введите строку откудa:"))
-    y1 = int(input("введите столбец откудa:"))
-    x2 = int(input("введите строку куда:"))
-    y2 = int(input("введите столбец куда:"))
+def winner():
+    #По горизонтали X
+    if area[0][0]["text"] == "X" and area[0][1]["text"] == "X" and area[0][2]["text"] == "X":
+        return "X"
+    elif area[1][0]["text"] == "X" and area[1][1]["text"] == "X" and area[1][2]["text"] == "X":
+        return "X"
+    elif area[2][0]["text"] == "X" and area[2][1]["text"] == "X" and area[2][2]["text"] == "X":
+        return "X"
+    
 
-    if area[x2][y2] == "*":
-        area[x2][y2] = area[x1][y1]
-        area[x1][y1] = "*"
-    #print1()
+    #По диагонали X
+    if area[0][0]["text"] == "X" and area[1][1]["text"] == "X" and area[2][2]["text"] == "X":
+        return "X"
+    elif area[0][2]["text"] == "X" and area[1][1]["text"] == "X" and area[2][0]["text"] == "X":
+        return "X"
+    
+    #По вертикали X
+    if area[0][0]["text"] == "X" and area[1][0]["text"] == "X" and area[2][0]["text"] == "X":
+        return "X"
+    elif area[0][1]["text"] == "X" and area[1][1]["text"] == "X" and area[2][1]["text"] == "X":
+        return "X"
+    elif area[0][2]["text"] == "X" and area[1][2]["text"] == "X" and area[2][2]["text"] == "X":
+        return "X"
+    
+    
+    #По горизонтали 0
+    if area[0][0]["text"] == "0" and area[0][1]["text"] == "0" and area[0][2]["text"] == "0":
+        return "0"
+    elif area[1][0]["text"] == "0" and area[1][1]["text"] == "0" and area[1][2]["text"] == "0":
+        return "0"
+    elif area[2][0]["text"] == "0" and area[2][1]["text"] == "0" and area[2][2]["text"] == "0":
+        return "0"
+    
+    #По диагонали 0
+    if area[0][0]["text"] == "0" and area[1][1]["text"] == "0" and area[2][2]["text"] == "0":
+        return "0"
+    elif area[0][2]["text"] == "0" and area[1][1]["text"] == "0" and area[2][0]["text"] == "0":
+        return "0"
+    
+    #По вертикали 0
+    if area[0][0]["text"] == "0" and area[1][0]["text"] == "0" and area[2][0]["text"] == "0":
+        return "0"
+    elif area[0][1]["text"] == "0" and area[1][1]["text"] == "0" and area[2][1]["text"] == "0":
+        return "0"
+    elif area[0][2]["text"] == "0" and area[1][2]["text"] == "0" and area[2][2]["text"] == "0":
+        return "0"
+    return ("")
 
 
-def print1():
-    print(area[0])
-    print(area[1])
+def push(but):
+    global turn
+    print(turn)
+    if (turn % 2) == 0:
+        turn_char = "0" 
+    else:
+        turn_char = "X"
+    print (f"Совершают ход: {turn_char}")
+    if but ["text"] is "":
+        but["text"] = turn_char
+        turn = turn + 1
+        if winner() == "x":
+            print("Победили крестики!")
+        if winner() == "0":
+            print("победили нолики:")
+        if winner() == "":
+            print("Ничья!")
+    #turn += 1 
 
 
-area = [["стол", "стул", "шкаф"],
-        ["стул", "*", "кресло"]]
+window = tk.Tk()
+window.title("Крестики нолики")
+window.geometry("300x300")
+window.resizable(False, False)
+area = []
+turn = 1
+#print(push())
+
+for x in range (3):
+    area.append([])
+    for y in range (3):
+        button = tk.Button(window, text = "", width= 13, height = 6)
+        area[x].append(button)
+        area[x][y].place(x = x * 100, y = y * 100)
+        area[x][y]["command"] = lambda selected_button = button: push(selected_button)
 
 
 
-while area[0][2] != "кресло" and area[1][2] != "шкаф":
-    print1()
-    turn()
-print("вы выиграли!")
+
+print(area)
+
+
+window.mainloop()
