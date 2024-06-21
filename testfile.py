cd6187eda7815524730eed9d2c77b94e610f5099
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-06-22 00:14:39 +0700

    123

--- testfile.py
+++ testfile.py
@@ -1,6 +1,6 @@
 import psycopg
 
-conn = psycopg.connect(dbname="littlebird1", user="user1", password="!Yh506Qpjm", host="79.174.88.184", port="17490")
+conn = psycopg.connect(dbname="littlebird1", user="user1", password="!Yh506Qpjm@", host="79.174.88.184", port="17490")
 cursor = conn.cursor()
 
 cursor.execute("SELECT * FROM people")
