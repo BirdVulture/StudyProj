ce4b095d241f6ff8be971b25061c4b3da9d3496b
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-06-30 21:11:42 +0700

    30.06

--- lvl2/lesson1/lesson1.py
+++ lvl2/lesson1/lesson1.py
@@ -2,17 +2,31 @@
 #pip install arcade
 #https://pypi.org
 #https://api.arcade.academy/en/latest/api/window.html#arcade.Window
-
+#https://api.arcade.academy/en/latest/arcade.color.html
 import arcade 
+
+class Ball(arcade.Sprite):
+       pass
 
 class Game(arcade.Window):
     def __init__ (self, width, height, title):
         super().__init__(width, height, title)
+        self.ball = Ball('/Users/olegz/Documents/GitHub/StudyProj/lvl2/lesson1/ball.png', 0.1)
+        self.ball.center_x = 300
+        self.ball.center_y = 300
+
+
+        
     def on_draw(self):
         #self.background_color = (255, 0, 0)
-        self.clear((255, 0, 0))
+        self.clear((255, 255, 255))
+        self.ball.draw()
 
-window = Game(500, 600, "Ping Pong")
+
+
+
+
+window = Game(600, 600, "Ping Pong")
 
 
 
