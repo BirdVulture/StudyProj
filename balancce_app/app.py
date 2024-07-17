dc98b3fdb2e7da41661c105a1bdb95d519e86e4a
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-07-18 01:17:35 +0700

    123

---
+++ balancce_app/app.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from tkinter import ttk
+ 
+window = Tk()
+
+window["bg"] = "white"
+window.title("My Balance")
+window.geometry("600x650")
+window.minsize(600,650)   # минимальные размеры
+window.maxsize(600,650)   # макисмальные размеры'''
+
+
+
+#Кнопки
+btn_income = Button(text="Приход") # создаем кнопку из пакета tkinter
+btn_income.place(x=20, y=120)    # размещаем кнопку в окне
+
+btn_outcome = Button(text="Расход") # создаем кнопку из пакета tkinter
+btn_outcome.place(x=120, y=120)    # размещаем кнопку в окне
+
+#поле ввода
+input_data = Entry(text="Приход")
+input_data.place(x=20, y=20, width= 200)
+
+ 
+window.mainloop()
