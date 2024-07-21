4ca24f141bf03f5ebe11f3fc1547ab660a149af8
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-07-21 21:53:28 +0700

    !

--- lvl2/lesson1/lesson1.py
+++ lvl2/lesson1/lesson1.py
@@ -12,37 +12,45 @@ CHANGE_X = 5
 CHANGE_Y = 5
 
 class Ball(arcade.Sprite):
-        def update(self):
-            self.center_x += self.change_x
-            self.center_y += self.change_y
-            if self.right >= SCREEN_WIDTH or self.left <= 0:
-                self.change_x = -self.change_x
-            if self.bottom <= 0 or self.top >= SCREEN_HEIGHT:
-                self.change_y = -self.change_y
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+        if self.right >= SCREEN_WIDTH or self.left <= 0:
+            self.change_x = -self.change_x
+        if self.bottom <= 0 or self.top >= SCREEN_HEIGHT:
+            self.change_y = -self.change_y
             
-           
+class Bar(arcade.Sprite):
+    def update(self):
+        self.center_x += self.change_x
+        if self.right >= SCREEN_WIDTH:
+            self.right = SCREEN_WIDTH
+        if self.left <= 0:
+            self.left = 0
             
 
 class Game(arcade.Window):
     def __init__ (self, width, height, title):
         super().__init__(width, height, title)
         self.ball = Ball('/Users/olegz/Documents/GitHub/StudyProj/lvl2/lesson1/ball.png', 0.1)
+        self.bar = Bar('/Users/olegz/Documents/GitHub/StudyProj/lvl2/lesson1/bar.png', 0.1)
         self.setup()
     def setup(self):
         self.ball.center_x = SCREEN_WIDTH / 2
         self.ball.center_y = SCREEN_HEIGHT / 2
         self.ball.change_x = CHANGE_X
         self.ball.change_y = CHANGE_Y
+        self.bar.center_x = SCREEN_WIDTH / 2
+        
     def update(self, delta_time):
-         self.ball.update()
-         
-
+        self.ball.update()
+        self.bar.update()
         
     def on_draw(self):
         #self.background_color = (255, 0, 0)
         self.clear((255, 255, 255))
         self.ball.draw()
-
+        self.bar.draw()
 
 
 
