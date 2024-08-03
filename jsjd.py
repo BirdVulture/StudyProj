95df639e027ce1595f990993a87456ac91de6ebe
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-08-03 13:34:01 +0700

    school entry test

--- jsjd.py
+++ jsjd.py
@@ -1,4 +1,29 @@
-x = int(input())
-hours = x % 60
-minutes = x // 60
-print(f"{hours} {minutes}")
+list = []
+process = True
+summ = 0
+maxx = 0
+minn = 1000
+
+
+while process == True:
+    money = int(input())
+    list.append(money)
+    if money == 0:
+        process = False
+
+for i in range(len(list)):
+    
+    if maxx < list[i]:
+        maxx = list[i]
+
+for i in range(len(list)):
+    
+    if minn > list[i] and list[i] != 0:
+        minn = list[i]
+
+summ = maxx - minn
+
+print(list)
+print(maxx)
+print(minn)
+print(summ)
