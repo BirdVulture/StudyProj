6e565f60c4920bc078d93d535de94a78c8721836
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-08-05 22:18:27 +0700

    new json

--- jsjd.py
+++ jsjd.py
@@ -1,29 +1,29 @@
-list = []
-process = True
-summ = 0
-maxx = 0
-minn = 1000
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
+def plot_points(x, y, z):
+    # Создание 3D-графика
+    fig = plt.figure(figsize=(5, 5))
+    ax = fig.add_subplot(111, projection='3d')
 
-while process == True:
-    money = int(input())
-    list.append(money)
-    if money == 0:
-        process = False
+    # Нанесение точек
+    ax.scatter(x.flatten(), y.flatten(), z.flatten(), s=10)
 
-for i in range(len(list)):
-    
-    if maxx < list[i]:
-        maxx = list[i]
+    # Указание заголовков и меток
+    ax.set_title(f'{np.sum(np.array(np.shape(x)) > 1)}D Parameter Space')
+    ax.set_xlabel('X-axis')
+    ax.set_ylabel('Y-axis')
+    ax.set_zlabel('Z-axis')
 
-for i in range(len(list)):
-    
-    if minn > list[i] and list[i] != 0:
-        minn = list[i]
+    # Вывод графика
+    plt.show()
 
-summ = maxx - minn
-
-print(list)
-print(maxx)
-print(minn)
-print(summ)
+# Генерация данных для 3D-пространства
+points = np.linspace(0, 1, 10)
+x, y, z = np.meshgrid(points, 1, 1)
+plot_points(x, y, z)
+x, y, z = np.meshgrid(points, points, 1)
+plot_points(x, y, z)
+x, y, z = np.meshgrid(points, points, points)
+plot_points(x, y, z)
