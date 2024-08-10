bfe456e2e2ae7a25db59ef4454d4c09b44b25411
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-08-10 21:02:56 +0700

    123

--- balancce_app/app.py
+++ balancce_app/app.py
@@ -18,7 +18,7 @@ btn_outcome.place(x=120, y=120)    # размещаем кнопку в окне
 
 #поле ввода
 input_data = tk.Entry(text="Приход", background = bg)
-input_data.place(x=20, y=20, width= 20)
+input_data.place(x=20, y=20, width= 200)
 
  
 window.mainloop()
