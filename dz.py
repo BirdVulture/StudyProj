2a555d41bb5ec29e43a10c3d211c5e7843710e77
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-08-17 21:35:13 +0700

    123

---
+++ dz.py
@@ -0,0 +1,16 @@
+coord = 0
+change_x = 5
+def moving(coord, change_x):
+    coord = coord + change_x
+    
+    
+        
+print(coord)
+
+'''
+дз
+написать консольное приложение в диапозоне от 0 до 100, чтобы оно ходило с определенным шагом бесконечно в диапозоне от 0 до 100
+'''
+i = 1
+while i <= 10:
+    print("hello")
