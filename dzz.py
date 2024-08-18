2d62660e4daf17d4bc19a5c1933f1ddc1eb69a7f
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-08-18 21:09:18 +0700

    12

--- dzz.py
+++ dzz.py
@@ -1,3 +1,49 @@
+'''
 for i in range(0,10):
     print("hello")
     i += 1
+
+ 
+i = 1
+while i <= 10:
+    print("hello")
+    i += 1
+    '''
+
+coord = 0
+change = 10
+flag = "y1"
+while flag == "y" :
+    if coord >= 0:
+        print("hello1")
+        coord = coord + change
+    elif coord <= 10:
+        print("hello2")
+        coord = coord - change
+
+val = 10
+#val = -val
+val2 = -val
+
+1. 
+coord = 0
+change = 10
+flag = "y"
+2.
+coord = 10
+change = 10
+flag = "y"
+3.
+coord = 10
+change = 10
+flag = "y"
+
+x = 2
+y = x + 3
+o = x - x * 2
+
+
+x = 3
+k = x * x
+print(k)
+print(x)
