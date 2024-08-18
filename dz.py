ebed3654aabe19e3265c93eadd117522c91a7343
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-08-18 14:11:31 +0700

    123

--- dz.py
+++ dz.py
@@ -1,36 +1,13 @@
+flag = "y"
+
 coord = 0
-change_x = 5
-def moving(coord, change_x):
-    coord = coord + change_x
-    
-    
-        
-#
-# print(coord)
+change = 33
 
-'''
-дз
-написать консольное приложение в диапозоне от 0 до 100, чтобы оно ходило с определенным шагом бесконечно в диапозоне от 0 до 100
-'''
-
-i = 1
-while i <= 10:
-    print("hello")
-    i = i + 1
-
-'''for i in range(49, 100):
-    print(i)'''
-
-"""i = 10
-while i += 10:
-    print(i)
-"""
-
-
-number = 1
- 
-while number != 5:
-    print(f"number = {number}")
-    number += 2
-print("Работа программы завершена")
+while flag == "y":
+    if coord <= 100 and coord >= 0:
+        coord = coord + change
+        print(coord)
+        flag = str(input("продолжать? y/n "))
+    if coord >= 100 or coord <= 0:
+        change = - change
 
