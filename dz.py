8459d0207fb27cc96f1b4d1b3a8099d710dcb6c6
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-08-18 14:30:10 +0700

    1234

--- dz.py
+++ dz.py
@@ -1,13 +1,16 @@
+#Папа
+
 flag = "y"
 
 coord = 0
 change = 10
 
 while flag == "y":
-    if coord <= 100 and coord >= 0:
-        coord = coord + change
-        print(coord)
-        flag = str(input("продолжать? y/n "))
+    
+    coord = coord + change
+    print(coord)
+    flag = str(input("продолжать? y/n "))
+
     if coord >= 100 or coord <= 0:
         change = - change
 
