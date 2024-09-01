34a71edd845efae53c71d0f9ffaf57fbd6cb05c3
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-09-01 18:15:36 +0700

    234

--- tst.py
+++ tst.py
@@ -1,2 +1,17 @@
-data = str(input())
-print(data)
+backpack = []
+n = int(input())
+for i in range(n):
+    a = int(input())
+    backpack.append(a)
+
+print(backpack)
+
+heavy = backpack[0]
+
+for i in range(len(backpack)):
+    if backpack[i] > heavy:
+        heavy = backpack[i]
+
+print(heavy)
+        
+        
