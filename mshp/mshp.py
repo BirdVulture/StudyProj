af0bc55b422d329983ec7587982511a40a24d4e4
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-09-08 21:02:27 +0700

    123

---
+++ mshp/mshp.py
@@ -0,0 +1,13 @@
+#урок1
+'''
+x = str(input("What do you love? "))
+print(f"I love {x} too")
+'''
+
+n = int(input())
+if n > 10:
+    print("эксперт")
+elif n >= 4:
+    print("уверенный пользователь")
+else:
+    print("любитель")
