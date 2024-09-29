2e35e90d3f158cb9ac00643ecc654f36118b2880
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-09-29 20:37:13 +0700

    1234

--- mshp/mshp.py
+++ mshp/mshp.py
@@ -1,14 +1,14 @@
 #урок1
 
-
-
-list = []
-for i in range(5):
-    a = float(input())
-    b = round(a)
-    if (a - b) >= 0.5:
-        b = b + 1
-    list.append(b)
-print(list)
-for i in range(len(list)):
-    print(list[i])
+def num_digits(n):
+    num = 0
+    while n != 0:
+        num %
+        n //= 10
+        num = num + 1
+    return num
+    
+    
+a = int(input())
+b = num_digits(a)
+print(b)
