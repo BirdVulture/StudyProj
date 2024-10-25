767f717d3fe585aa4fe99b4fd7028f416d7aa22f
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-10-25 19:17:42 +0700

    1234

---
+++ Swift Hudson/FirstApp/BalanceApp/BalanceApp/BalanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalanceAppApp.swift
+//  BalanceApp
+//
+//  Created by Oleg Z on 25.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct BalanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift Hudson/FirstApp/BalanceApp/BalanceApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BalanceApp
+//
+//  Created by Oleg Z on 25.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
