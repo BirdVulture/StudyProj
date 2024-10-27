775e367d4b3b0ad355fc5d9a73407e0b8f0b190b
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-10-27 20:34:48 +0700

    1234

--- mshp/mshp.py
+++ mshp/mshp.py
@@ -1,27 +1,34 @@
 #урок1
+import random
+
+def throw_green_dice():
+    #бросок зеленого кубика
+    x = random.randint(1, 6)
+    answer = 0
+    if x == 1 or x == 2 or x == 3:
+        answer = 1
+        print("преступник сдался")
+    elif x == 4 or x == 5:
+        answer = 0
+        print("преступник сбежал")
+    elif x == 6:
+        answer = 1000
+        print("прогремел выстрел")
+
+    return answer
+
+#throw_green_dice()
+
+def pick_dice(broski):
+    green = broski // 10
+    red =  broski % 10
+    n = green + red
+    y = random.randint(1, n)
+    if 1 <= y <= green:
+        print("вытащили зелёный!")
+    else:
+        print("вытащили красный")
+pick_dice(49)
 
 
-min = int(input())
-max = int(input())
 
-
-def count(n):
-   global min
-
-   #n - это номер слоя
-
-   if n == min:
-
-       return min
-
-   else:
-        x = count(n-1) + 1
-        return x
-
-#для 101 слоя вызов такой
-
-print(count(max))
-
-
-
-            
