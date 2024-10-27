5e7c62801a002dd75102017497a769a9abda1428
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-10-27 14:35:41 +0700

    1234

--- mshp/mshp.py
+++ mshp/mshp.py
@@ -1,13 +1,11 @@
 #урок1
 
-def fib(n):
-    if n == 2:
-        return 1
-    elif n == 1:
-        return 1
-    else:
-        return fib(n - 1) + fib(n - 2)
-
-n = int(input())
-print(fib(n))
+def rev():
+    x = int(input())
+    if x != 0:
+        rev()
+    print(x)
+ 
+    
+rev()
             
