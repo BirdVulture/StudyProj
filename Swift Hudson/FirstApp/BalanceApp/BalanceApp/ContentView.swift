b437dc0a42785b44170ea5c656d579cd94b02b92
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-10-27 12:38:10 +0700

    1234

--- Swift Hudson/FirstApp/BalanceApp/BalanceApp/ContentView.swift
+++ Swift Hudson/FirstApp/BalanceApp/BalanceApp/ContentView.swift
@@ -5,19 +5,48 @@
 //  Created by Oleg Z on 25.10.2024.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var destinations: [Destination]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(destinations) { destination in
+                    VStack(alignment: .leading) {
+                        Text(destination.name)
+                            .font(.headline)
+                    
+                        Text(destination.date.formatted(date: .long, time: .shortened ))
+                }
+                    .navigationTitle("BalancseApp")
+                    .toolbar{
+                        Button("Add Samples", action: addSamples)
+                    }
+                }
+            }
         }
-        .padding()
     }
+    
+    func addSamples() {
+        let rome = Destination(name: "Rome")
+        let florence = Destination(name: "Florece")
+        let naples = Destination(name: "Naples")
+        
+        modelContext.insert(rome)
+        modelContext.insert(florence)
+        modelContext.insert(naples)
+    }
+    
 }
+
+
+
+
+
 
 #Preview {
     ContentView()
--- Swift Hudson/testing.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ Swift Hudson/testing.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,6 +1,14 @@
 import Foundation
 
-var greeting = "Hello, playground"
+var task: [[String: Any]] = [
+    ["deskription": "Call home", "isCompleted": true],
+    ["deskription": "Call son", "isCompleted": false]
+]
+
+var description = task[1]["deskription"]
+
+
+
 
 
 
