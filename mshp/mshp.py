46e00eaa6258500956cf3a341682141c967f116c
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-11-11 10:22:29 +0700

    1234

--- mshp/mshp.py
+++ mshp/mshp.py
@@ -1,15 +1,3 @@
 #урок1
-
-min = int(input())
-max = int(input())
-
-def rec(max):
-    global min
-    x = max
-    if x != min:
-        rec(max - 1)
-    print(x)
- 
-    
-rec(max)
-            
+a = str(input())
+print(a[5:0])
