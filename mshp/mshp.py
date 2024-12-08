acd7d6951c001e0538b8f0b04ec6ca6a6d3823c9
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-12-08 22:32:24 +0700

    123

--- mshp/mshp.py
+++ mshp/mshp.py
@@ -1,15 +1,21 @@
+import re
+def removeHTML(text):
+    return re.sub(r'\<[^>]*\>', '', text)
+from urllib import request
 
+# подключаемся к странице в Интернете и выкачиваем её код себе в программу:
+connect = request.urlopen("https://www.cbr.ru/key-indicators/")
+text = connect.read().decode()
 
-def replace_name():
-    #имя
-    w = str(input())
-    #строка
-    w1 = str(input())
-    if w1.find(w) == True:
-        w1 = w1.replace(w, "[username]")
-    print(w1)
+# зацепка в html коде, которая указывает, где начинается информация про евро:
+what = 'Евро'
 
-replace_name()
+# ищем, с какого по индексу символа начинается эта старт-зацепка:
+start = text.find(what)
 
+# зацепка в html коде, которая указывает, где заканчивается информация про евро:
+end = text.find("евро", start+len(what))
 
-
+# Вырезаем кусочек кода с начальной ко конечную зацепки и убираем лишний html код вокруг числа (курса евро):
+text1 = text[start:end]
+print("Курс ЕВРО =", removeHTML(text1))
