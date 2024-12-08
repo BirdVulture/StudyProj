2e9e83bd276ccbc9ac09c362040eafdc382a1bc7
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-12-08 16:04:28 +0700

    1234

--- Swift Lessons/StudyApp/StudyApp/ContentView.swift
+++ Swift Lessons/StudyApp/StudyApp/ContentView.swift
@@ -13,60 +13,58 @@
 //
 
 import SwiftUI
-import SwiftData
 
 
 
 struct ContentView: View {
-    @State private var showHello = true
-    @State private var showDetail = false
+    @State private var users = ["Paul", "Taylor", "Adele"]
+    @State private var selection: String? = nil
     
     
+
     var body: some View {
-        VStack(spacing: 100) {
-            Toggle(isOn: $showHello) {
-                Text("Show Hello")
-            } .padding()
-            if showHello {
-                Text("Hello")
-            }
-            
-            Button(action: {
-                self.showDetail.toggle()
-            }, label: {
-                Image(systemName: "minus")
-            })
-            if showDetail {
-                Text("somthing")
-            }
-            
-            
-            
-            Text("Hello world")
-                .foregroundColor(.white)
-                .frame(width: 200, height: 200)
-                .padding(.all, 50)
-                .lineLimit(nil)
-                .truncationMode(.middle)
-                .font(.largeTitle)
-                .multilineTextAlignment(.center)
-                .background(Color.gray)
-                .padding(.all, 50)
-                .background(Color.indigo)
-            Image(systemName: "minus.circle")
-                .padding(30)
-        }
         
-    }
+            VStack {
+                Spacer()
+                HStack {
+                    Button("Add data") {
+                        users.append("A")
+                    }
+                  
+                }
+                Spacer()
+                List {
+                    ForEach(users, id: \.self) { user in
+                        NavigationLink {
+                            Text(user)
+                        }
+                        label: {
+                            HStack {
+                                Text("User \(user)")
+                                Spacer()
+                                Text("Delete")
+                                    .onTapGesture {
+                                        print("OOk")
+                                    }
+                            }
+                        }
+                        
+                    }
+                }
+                
+                        
+                }
+                
+            }
+        
+         
 }
 
+    
 
-
-
-
-
-
-
+   
+    
+   
 
 
 
@@ -81,4 +79,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-                                
+
