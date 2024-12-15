8bc5bc5b20901ad5ee17a461d467804cec431a99
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-12-15 20:27:29 +0700

    1234

---
+++ Swift.Study/MyPlayground.playground/Contents.swift
@@ -0,0 +1,113 @@
+
+// КОНСТАНТЫ И ПЕРЕМЕННЫЕ
+
+let greetings = "Hello" // Константа
+
+var direction = "Left" // Переменная
+
+direction = "Right" // Переназначение переменной
+
+// ТИПЫ ДАННЫХ
+
+var number: Int = 1
+
+var numDouble: Double = 1.5
+var numFloat: Float = 1.5
+var greeting1: String = "Hello "
+var greeting2: String = "World"
+
+var greetingsum = greeting1 + greeting2 // Конкатенация строк
+
+var greeting3 = "Hello, \(greeting2)" // Интерполяция строк
+
+print(greeting3)
+
+var areYouHappy: Bool = true
+
+//БАЗОВЫЕ ОПЕРАТОРЫ
+
+//Унарные: префиксные -a, !b постфиксные с!
+//Бинарные: инфиксные a + b
+//Тернарные: a ? b : c
+
+//Оператор присваивания
+var a = 12
+
+var b = 5
+
+//b = a
+
+// Арифметические операторы +, -, *, /, % (остаток от деления)
+
+var c = a + b
+c = a - b
+c = a * b
+c = a / b
+
+c = a % b
+
+//Составные операторы присваивания +=, -=
+
+c += 2 // c = c + 2
+
+// Счетчик
+
+var counter = 0
+
+counter += 1
+
+// Операторы сравнения ==, !=, >, <, <=, >=
+
+c > a
+
+//Тернарный условный оператор
+//выражение ? действие 1 : (иначе) действие 2
+
+//с помощью if
+if a == b {
+    print("a = b")
+} else {
+    print("a != b ")
+}
+// с помощью тернарного оператора
+a == b ? print("a = b") : print("a != b ")
+
+// Оператор замкнутого/закрытого (a...b) диапазона и полузамкнутого/полузакрытого диапазона (a..<b)
+
+
+// Логические операторы !a (не равно) && (И) || (ИЛИ)
+
+let isTheWeatherGood = true
+
+areYouHappy = true
+
+if areYouHappy && isTheWeatherGood {
+    print("Go outside")
+} else {
+    print("Stay home")
+}
+
+// УСЛОВИЯ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
