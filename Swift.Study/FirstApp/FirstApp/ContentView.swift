261b4a2e0a55fef4d970a52d0b5ac807b4a44247
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2024-12-20 16:19:53 +0300

    1234

---
+++ Swift.Study/FirstApp/FirstApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FirstApp
+//
+//  Created by Oleg Z on 20.12.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    
+
+    var body: some View {
+        
+        
+        Text("Hello, Oleg")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift.Study/FirstApp/FirstApp/FirstAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Oleg Z on 20.12.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FirstAppApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        
+    }
+}
