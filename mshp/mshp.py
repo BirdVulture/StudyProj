df3807654eb9a792c87441876b99f5d0bbf52292
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2025-02-09 14:09:12 +0700

    123

--- mshp/mshp.py
+++ mshp/mshp.py
@@ -1,4 +1,30 @@
-a = int(input())
-b = a % 100
-if b == 14:
-    a =+ 1
+a = input()
+b = a.split(" ")
+
+def superman_int(x):        #функция для превращения списка строк в список чисел
+    for i in range(len(x)):
+        x[i] = int(x[i])
+    return x
+
+
+def superman_remove2(x):    #функция для удаления двоек
+    x.remove(2)
+    return x
+
+
+def superman_average(x):
+    if len(x) == 0:
+        print("К сожалению, у вас были только двойки")
+    else:
+        summ = 0
+        for i in range(len(x)):
+            summ = summ + x[i]
+        aver = summ / len(b)
+        print(round(aver, 2))
+
+
+c = superman_int(b)
+d = superman_remove2(c)
+print(c)
+print(d)
+#superman_average(d)
