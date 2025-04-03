9bff2436bc8e389e0ec4e5d5f70e5316d9afc145
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2025-04-03 22:48:59 +0700

    1234

--- testTwo.py
+++ testTwo.py
@@ -1,9 +1,24 @@
-a = 3.6
-b = round(a)
-d = a - b
-print(d )    
+class Calculator:
+    num1 = 0
+    num2 = 0
 
-#Новая строка
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
 
-#Новая строка2
+    def addition (self, num1, num2):
+        sum = num1 + num2
+        return sum
+    def subtraction (self, num1, num2):
+        sum = num1 - num2
+        return sum
 
+
+c  = Calculator(0, 0)
+
+a= int(input())
+b= int(input())
+
+
+print(c.addition(a, b))
+print(c.subtraction(a, b))
