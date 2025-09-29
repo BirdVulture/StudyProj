5375f11a8696c7da1a42a35d0d45a46178475d27
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2025-09-29 21:23:49 +0700

    Update testTwo.py

--- testTwo.py
+++ testTwo.py
@@ -1,11 +1,35 @@
 
+class Observed:
+    def __init__(self):
+        self.observer = Coordinator() #жестко закрепляем наблюдателя
+        self.state = None
+    
+
+    #функция для передачи состояния наблюдателю
+    def notifyObserver(self, state):
+        self.observer.updateDataFromObserved(state)
+        print("notify ok")
+
+    #функция для изменения состояния и передачи его
+
+    def setState(self, value):
+        self.state = value
+        self.notifyObserver(self.state)
+        print("setState ok")
+        
+
+
+
 
 class Coordinator:
-    executor = ()
-    responseData = ()
+
+    #связь через свойства
+    
     
     def __init__(self):
         self.executor = Executor()
+        self.responseData = None
+        self.stateObserved = None
 
     def callFunc(self):
         self.executor.printing()
@@ -13,6 +37,17 @@ class Coordinator:
     def takeAnswer(self):
         self.responseData = self.executor.classData
         print(self.responseData)
+
+
+    #Функция, которую вызывает наблюдаемый при изменении состояния
+    def updateDataFromObserved(self, state):
+        
+        self.stateObserved = state
+        print(self.stateObserved)
+        print("update ok")
+
+
+
 
 class Executor:
 
@@ -29,9 +64,21 @@ class Executor:
 
 
 
-
+'''
 testObject = Coordinator()
 
 testObject.callFunc()
 testObject.takeAnswer()
+'''
+
+testObject2 = Observed()
+
+testObject2.setState("Data")
+
+
+
+
+
+
+
     
