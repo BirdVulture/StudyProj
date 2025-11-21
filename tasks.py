49c3b991888808d312f7054e90b0d79db828ba3d
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2025-11-21 20:55:28 +0700

    1234

--- tasks.py
+++ tasks.py
@@ -17,7 +17,7 @@ class Vehicle:
         self._mileage = self._mileage + distance
             
     def display_info(self):
-        print(f"""Марка: {self.brand}\nМакс. скорость: {self._max_speed} км/ч\nПробег: {self._mileage} км""")
+        return f"Марка: {self.brand} \nМакс. скорость: {self._max_speed} км/ч \nПробег: {self._mileage} км"
 
 class Car(Vehicle):
     pass
@@ -29,3 +29,4 @@ v1 = Vehicle("Tesla", 100)
 
 print(v1.get_max_speed())
 print(v1.display_info())
+print(v1.vehicles_created)
